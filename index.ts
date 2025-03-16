15138e9b2e258aa951c33c4f98cb3eec734be0af
orelsprojects <orelsmail@gmail.com>
2025-03-16 12:57:20 +0200

    note

--- index.ts
+++ index.ts
@@ -289,7 +289,7 @@ exports.handler = async (event: LambdaEvent): Promise<LambdaResponse> =>
 //   url: "https://giacomofalcone.substack.com/",
 // });
 
-scrapeForever("post");
+scrapeForever("note");
 
 // For AWS Lambda
 // export { handler };
